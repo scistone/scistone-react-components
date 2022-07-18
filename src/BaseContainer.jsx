e01edade3aa8baf2d2beec357c7cf597e0fe647b
Berk Cohadar <berkcohadar1@gmail.com>
2022-07-18 17:58:14 -0400

    Navbar items added. SASS integrated. Scistone logo added.

--- src/BaseContainer.jsx
+++ src/BaseContainer.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-
+import logo from './images/scistone_logo.png'
+import menuData from "./data/menu_data.json"
 export const BaseContainer = () => {
     return(
         <div className='navigation-bar'>
+            <div className='navigation-bar--logo'>
+                <img src={logo}></img>
+                <p>Scistone</p>
+            </div>
+            <div className='navigation-bar--items'>
+                {menuData.menu_data.map((item,index) => {
+                    return (
+                        <p index={index}>{item}</p>
+                    )
+                })}
+            </div>
+            <div className='navigation-bar--extra'></div>
         </div>
     );
 };
