f893747e4d72a35a6689eca548281c87c3cb5de0
degerahmet <adeger34@gmail.com>
2022-08-09 23:15:05 -0400

    Form, FormItem, Input added.

--- src/components/Navbar/NavItems/index.jsx
+++ src/components/Navbar/NavItems/index.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function NavItems(props,) {
     return (
         <div className='navigation-bar--items'>
-            {props.children}
+            {props.children[0]}
+            {props.children[1]}
+            {props.children[2]}
+            {props.children[3]}
+            {props.children[4]}
+            {props.children[5]}
         </div>
     )
 }
